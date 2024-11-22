aeb6ada0902d90891ad2f6dd35e9238a49b01a9c
shlomionn <shlomionn@gmail.com>
2024-11-22 21:47:53 +0200

    added basic vector operands (push,pop etc)

--- task3/Vector.cpp
+++ task3/Vector.cpp
@@ -2,8 +2,7 @@
 #include <iostream>
 #define MIN_SIZE 2
 
-
-Vector::Vector(int n = 10)
+Vector::Vector(int n)
 {
 	if (n < MIN_SIZE)
 	{
@@ -49,3 +48,101 @@ bool Vector::empty() const
 {
 	return _size == 0;
 }
+//change the capacity
+void Vector::reserve(const int n)
+{
+	int i = 0;
+	int* arr = nullptr;
+	if (_capacity < n)
+	{
+		while (_capacity < n)
+		{
+			_capacity += _resizeFactor;
+		}
+		arr = new int[_capacity];
+		for (i = 0; i < _size; i++)
+		{
+			arr[i] = _elements[i];
+		}
+		this->~Vector();
+		_elements = arr;
+	}
+}
+
+//adds element at the end of the vector
+void Vector::push_back(const int& val)
+{
+	this->reserve(_size + 1);
+	_elements[_size] = val;
+	_size++;
+}
+
+//removes and returns the last element of the vector
+int Vector::pop_back()
+{
+	int temp = 0;
+	if (_size == 0)
+	{
+		std::cout << "error: pop from empty vector" << std::endl;
+		return -9999;
+	}
+	temp = _elements[_size - 1];
+	_elements[_size - 1] = NULL;
+	_size--;
+	return temp;
+}
+
+//change _size to n, unless n is greater than the vector's capacity
+void Vector::resize(int n)
+{
+	int i = 0;
+	this->reserve(n);
+	if (n > _size)
+	{
+		for (i = _size; i < n; i++)
+		{
+			_elements[i] = 0; //how can my program tell if it's deleted or newly created? hope you can answer me next time
+		}
+	}
+	else
+	{
+		for (i = n; i < _size; i++)
+		{
+			_elements[i] = NULL;
+		}
+	}
+	_size = n;
+}
+
+//assigns val to all elemnts
+void Vector::assign(const int val)
+{
+	int i = 0;
+	for (i = 0; i < _size; i++)
+	{
+		_elements[i] = val;
+	}
+}
+
+//same as above, if new elements added their value is val
+void Vector::resize(const int n, const int& val)
+{
+	int i = 0;
+	this->reserve(n);
+	if (n > _size)
+	{
+		for (i = _size; i < n; i++)
+		{
+			_elements[i] = val;
+		}
+	}
+	else
+	{
+		for (i = n; i < _size; i++)
+		{
+			_elements[i] = NULL;
+		}
+	}
+	_size = n;
+}
+
